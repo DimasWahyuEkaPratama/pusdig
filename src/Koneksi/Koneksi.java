31d51334e564cba7f6ab37ccdb7e6c19b9768223
DimasWahyuEkaPratama <dimaswahyuekapratama@gmail.com>
2026-01-20 09:01:41 +0700

    Update

---
+++ src/Koneksi/Koneksi.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Koneksi;
+import java.sql.*;//memanggil plugin sql
+import javax.swing.*;//memanggil mysql jdbc
+
+/**
+ *
+ * @author dppra
+ */
+public class Koneksi {
+     public static Connection KoneksiDB() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");//memanggil driver JDBC
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/perpusdigital", "root", "");
+            return conn;
+        } catch (ClassNotFoundException | SQLException e) {
+            JOptionPane.showMessageDialog(null, e);
+        }
+    return null;
+    }    
+}
---
+++ src/pusdig/session.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pusdig;
+
+/**
+ *
+ * @author dppra
+ */
+public class session {
+    private static String U_id;
+    private static String U_username;
+
+    public static String getU_id() {
+        return U_id;
+    }
+
+    public static void setU_id(String U_id) {
+        session.U_id = U_id;
+    }
+
+    public static String getU_username() {
+        return U_username;
+    }
+
+    public static void setU_username(String U_username) {
+        session.U_username = U_username;
+    }
+
+    public static void logout() {
+        U_id = null;
+        U_username = null;
+    }
+}
